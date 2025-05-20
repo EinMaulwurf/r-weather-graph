37f36381310b7f0f0164a5450fed300926d5c782
EinMaulwurf <github@sgeis.net>
2025-05-20 13:00:58 +0200

    Manually set links to correct DWD dataset, because rdwd package is currently broken

--- R/Retrieve_dwd.R
+++ R/Retrieve_dwd.R
@@ -1,9 +1,18 @@
 library(dplyr)
 library(rdwd) # Muss aktuellste Version sein! Version von CRAN eventuell veraltet. Verwende rdwd::updateRdwd() oder installiere von GitHub.
 
-# findID("Frankfurt/Main", exactmatch = FALSE)
+# findID("Frankfurt/Main", exactmatch = FALSE) # 1420
 
-link <- selectDWD("Frankfurt/Main", res = "daily", var = "kl", per = "hr")
+# rdwd::selectDWD() is currently broken
+# see https://github.com/brry/rdwd/issues/47#issuecomment-2893840683
+# I'm setting the links manually
+# link <- selectDWD("Frankfurt/Main", res = "daily", var = "kl", per = "hr")
+
+link <- c(
+  "ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/tageswerte_KL_01420_19350701_20241231_hist.zip", 
+  "ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/recent/tageswerte_KL_01420_akt.zip"
+)
+
 file <- dataDWD(link, read = FALSE, dir = "data", force = TRUE)
 clim <- readDWD(file, varnames = TRUE, hr = 4)
 
